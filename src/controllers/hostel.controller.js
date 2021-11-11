8192ddcbf0409914d18f2b981f2e9d68c0cbea05
spykelion <ndilionel6@gmail.com>
2021-11-11 01:27:32 +0100

    complete controller on hostel

--- src/controllers/hostel.controller.js
+++ src/controllers/hostel.controller.js
@@ -1,13 +1,14 @@
 const Hostel = require('../models/Hostel')
 
 module.exports = {
-   create: async (req, res, next) =>{
+   create: async (req, res, next) => {
     Hostel?.exists({hostelNumber:req.body.hostelNumber}).then(async(result)=>{
         if(!result){
             try {
                 const hostel = new Hostel({...req.body})
                 await hostel.save().then(result=>res.status(201).send(result))
                 .catch(err=>res.status(409).send(err))
+                console.log(req.body)
             } catch (error) {
                 console.log(error)
             }
@@ -20,19 +21,56 @@ module.exports = {
     }).catch(err=>console.error(err))
     },
 
-    getAll: async function(req, res, next) {
+    getAll: async (req, res, next) => {
         await Hostel.find({}).lean().then(result=>res.status(200).send(result))
-            .catch(err=>res.status(503).send(err))
-            
+            .catch(err=>res.status(503).send(err))     
     },
 
-    getOne: async(req,res) =>{
-        await Hostel.findOne({_id:req.params.id})
+    getOne: async(req,res) => {
+        await Hostel.findOne({_id:req.params.id}).lean()
             .then(result=>res.status(200).send(result))
             .catch(err=>res.status(404).json({
                 ...err,
                 message: "Not found"
             }))
+    },
+
+    deleteOne: async(req,res) =>{
+        await Hostel.deleteOne({_id:req.params.id})
+            .then(result=>res.status(200).send(result))
+            .catch(err=>res.status(404).json({
+                ...err,
+                message: "Not found"
+            }))
+    },
+
+    deleteAll: async(req,res) =>{
+        await Hostel.deleteAll({})
+            .then(result=>res.status(200).send(result))
+            .catch(err=>res.status(404).json({
+                ...err,
+                message: "Not found"
+            }))
+    },
+
+    update: async(req,res) =>{
+        Hostel?.exists({hostelNumber:req.body.hostelNumber}).then(async(result)=>{
+            if(result){
+                try {
+                    await Hostel.updateOne({hostelNumber:req.body.hostelNumber},{
+                        $set: req.body
+                    }).then(result=>res.status(201).send(result))
+                    .catch(err=>res.status(409).send(err))
+                } catch (error) {
+                    console.log(error)
+                }
+            } else {
+                res.status(409).json({
+                    message: "Resource Exist"
+                })
+            }
+            
+        }).catch(err=>console.error(err))
     }
 
 }
