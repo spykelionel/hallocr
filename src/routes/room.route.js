121b01f33261ac37e0e2d3bfc3cbce32b1fc1c0d
spykelion <ndilionel6@gmail.com>
2021-11-11 01:59:38 +0100

    create room route

---
+++ src/routes/room.route.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+const room = require('../controllers/room.controller')
+
+router.get('/', room.getAll)
+router.get('/:id', room.getOne)
+router.post('/', room.create)
+router.post('/:id', room.deleteOne)
+router.post('/', room.deleteAll)
+router.patch('/:id', room.update)
+
+module.exports = router
