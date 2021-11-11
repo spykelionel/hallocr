8170059ad783ce6184db0242806faa9d1badb5fc
spykelion <ndilionel6@gmail.com>
2021-11-11 01:42:41 +0100

    update route with CRUD

--- src/routes/hostel.route.js
+++ src/routes/hostel.route.js
@@ -7,5 +7,8 @@ const hostel = require('../controllers/hostel.controller')
 router.get('/', hostel.getAll)
 router.get('/:id', hostel.getOne)
 router.post('/', hostel.create)
+router.post('/:id', hostel.deleteOne)
+router.post('/', hostel.deleteAll)
+router.patch('/:id', hostel.update)
 
 module.exports = router
