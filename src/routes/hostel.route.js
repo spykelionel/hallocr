852d1790b58e3ed0a74f757a93168284a6f48423
spykelion <ndilionel6@gmail.com>
2021-11-11 01:47:06 +0100

    update route with CRUD

--- src/routes/hostel.route.js
+++ src/routes/hostel.route.js
@@ -3,7 +3,6 @@ const router = express.Router()
 
 const hostel = require('../controllers/hostel.controller')
 
-
 router.get('/', hostel.getAll)
 router.get('/:id', hostel.getOne)
 router.post('/', hostel.create)
