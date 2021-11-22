95aff7b52ed877151d5a3874f45846ab39ce0d4b
spykelion <ndilionel6@gmail.com>
2021-11-22 17:53:22 +0100

    move dependencies to prod dep

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,13 +7,16 @@ const user = require('../controllers/user.controller')
 // router.all('*', ()=>{
 //     // authenticate for any route benieth this route
 // })
-router.get('/', user.getAll)
+// router.get('/', user.getAll)
 // router.get('/uploads/:img', user.getImg)
-router.get('/:id', user.getOne)
-router.post('/',auth, user.upload, user.create)
+// router.get('/:id', user.getOne)
+// router.post('/',auth, user.upload, user.create)
+// router.delete('/:id',auth, user.deleteOne)
+// router.delete('/',auth, user.deleteAll)
+// router.patch('/:id',auth, auth,user.update)
+
+router.route('/').get(user.getAll).post(auth, user.create).delete(auth, user.deleteAll)
+router.route('/:id').get(user.getOne).patch(auth, user.update).delete(auth, user.deleteOne)
 router.post('/login', user.login)
-router.delete('/:id',auth, user.deleteOne)
-router.delete('/',auth, user.deleteAll)
-router.patch('/:id',auth, auth,user.update)
 
 module.exports = router
